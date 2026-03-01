d1de6deae4b0114b5273d7058062d59e1d41c2d9
John Patrick <johnpatrickdeguzman149@gmail.com>
2026-03-01 15:12:32 -0800

    Add project files.

---
+++ Job Applicant Tracking System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Job_Applicant_Tracking_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
